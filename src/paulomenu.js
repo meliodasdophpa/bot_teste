dc56355d519254dc13cd01db953bf1b101906510
meliodasdophpa <ultrassh12@gmail.com>
2021-02-04 08:58:52 -0300

    first commit

---
+++ src/paulomenu.js
@@ -0,0 +1,12 @@
+const paulomenu = (prefix, pushname) => {
+    return `◪ *Comandos 
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.paulomenu = paulomenu
